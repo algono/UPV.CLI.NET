de9fbf8df1adfd700489b95fafb667b055acc8c0
Alejandro Gómez <algono@inf.upv.es>
2025-06-05 16:46:14 +0200

    Improved VPN connect success message

--- UPV.CLI/VpnCommands.cs
+++ UPV.CLI/VpnCommands.cs
@@ -50,7 +50,7 @@ namespace UPV.CLI
             var process = VPNHelper.Connect(name);
 
             CmdHelper.WaitCheckAndOutput(process,
-                $"Successfully connected to VPN: {name}",
+                $"Successfully opened connection dialog for VPN: {name}.\nCheck your VPN settings to see if the connection was successful or not.\nAlternatively, you can use the \"[upv-cli] vpn check\" command to see if your current IP is in the UPV network.",
                 $"Failed to connect to VPN: {name}\nError: {{0}}",
                 $"Failed to start connection process for VPN: {name}");
         }
