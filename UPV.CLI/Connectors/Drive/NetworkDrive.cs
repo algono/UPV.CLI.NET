4ac792ae24d2cef736d9a979ca0fccba55b0496a
Alejandro Gómez <algono@inf.upv.es>
2025-06-06 18:25:39 +0200

    Open error message in English

--- UPV.CLI/Connectors/Drive/NetworkDrive.cs
+++ UPV.CLI/Connectors/Drive/NetworkDrive.cs
@@ -88,7 +88,7 @@ namespace UPV.CLI.Connectors.Drive
         public void Open()
         {
             if (IsConnected) Process.Start("explorer.exe", ConnectedDriveLetter!);
-            else throw new InvalidOperationException("El disco debe estar conectado para poder abrirlo");
+            else throw new InvalidOperationException("The drive must be connected in order to be opened");
         }
 
 
